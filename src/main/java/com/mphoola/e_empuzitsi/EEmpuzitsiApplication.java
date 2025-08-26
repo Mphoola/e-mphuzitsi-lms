1e4693d41a89e913e6c45686d86bff793e68e337
mphoola <promisemphoola2@gmail.com>
2025-08-27 00:01:46 +0200

    ":rocket:"

---
+++ src/main/java/com/mphoola/e_empuzitsi/EEmpuzitsiApplication.java
@@ -0,0 +1,13 @@
+package com.mphoola.e_empuzitsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EEmpuzitsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EEmpuzitsiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mphoola/e_empuzitsi/EEmpuzitsiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mphoola.e_empuzitsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EEmpuzitsiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
